f40eb47b4d736129bf4202e853c59bd9c94d7e49
basil <basil.mawejje@gmail.com>
2017-08-10 19:08:48 +0300

    Iterate through my_group

--- my_group.rb
+++ my_group.rb
@@ -6,3 +6,7 @@ person_3 = {name: "jane", gender: "female", age: 34}
 my_group << person_1
 my_group << person_2
 my_group << person_3
+
+my_group.sort_by { |x| x[:name] }.each do |x|
+  puts "#{x[:name]} is a #{x[:age]} old #{x[:gender]}."
+end
