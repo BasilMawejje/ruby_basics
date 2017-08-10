ac7c1dc6c31ab51b74a922f79beb2e606a6ed93d
basil <basil.mawejje@gmail.com>
2017-08-10 17:41:20 +0300

    Ruby basics

---
+++ my_group.rb
@@ -0,0 +1,8 @@
+my_group = []
+person_1 = {name: "basil", gender: "male", age: 23}
+person_2 = {name: "sheila", gender: "female", age: 50}
+person_3 = {name: "jane", gender: "female", age: 34}
+
+my_group << person_1
+my_group << person_2
+my_group << person_3
